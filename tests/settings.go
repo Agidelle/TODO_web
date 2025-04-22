fa349db2a749f95c5730ea33e476b083a7bd4212
Agidelle <agidelleds@gmail.com>
2025-04-22 22:02:13 +0300

    init test+web

---
+++ tests/settings.go
@@ -0,0 +1,7 @@
+package tests
+
+var Port = 7540
+var DBFile = "../scheduler.db"
+var FullNextDate = true
+var Search = true
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDUzNjEwNjQsImlhdCI6MTc0NTI3NDY2NH0.u5d4Mko3VvxYKEi_c8yNkoM2bygHB6PbZ3l8lMH506w`
